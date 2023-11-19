2ddab2abbef35ecd09de2d2d01212635837e44e9
Alexander Filimonov <a.filimonov@prequelapp.com>
2023-11-19 11:48:54 +0000

    bump pizzakit

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/PizzaKit/pizzakit",
-            from: "1.0.1"
+            from: "2.0.0"
         ),
         .package(
             url: "https://github.com/firebase/firebase-ios-sdk",
